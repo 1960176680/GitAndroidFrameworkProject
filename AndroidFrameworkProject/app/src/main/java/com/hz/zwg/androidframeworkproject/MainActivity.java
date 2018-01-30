1589b48bf91dcabe918d07880798fffdf16428df
Sun <1960176680@qq.com>
2018-01-30 11:13:57 +0800

    提交：
    我的Android框架项目构建

---
+++ AndroidFrameworkProject/app/src/main/java/com/hz/zwg/androidframeworkproject/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hz.zwg.androidframeworkproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        setContentView(R.layout.activity_main);
+
+    }
+}
